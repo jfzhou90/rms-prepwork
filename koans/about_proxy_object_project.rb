d3b724dbc852553734157f420ffdafa9042ab9a7
jfzhou90 <burninghot4jesus2005@gmail.com>
2018-08-26 12:03:15 -0400

    complete 277/282

--- koans/about_proxy_object_project.rb
+++ koans/about_proxy_object_project.rb
@@ -16,9 +16,30 @@ class Proxy
   def initialize(target_object)
     @object = target_object
     # ADD MORE CODE HERE
+    @messages = []
   end
 
   # WRITE CODE HERE
+  def called? (method_name)
+    @messages.include?(method_name)
+  end
+
+  def messages
+    @messages
+  end
+
+  def number_of_times_called(method_name)
+    @messages.count(method_name)
+  end
+
+  def method_missing(method_name, *args, &block)
+    if @object.respond_to? method_name
+      @messages.push method_name
+      @object.send method_name, *args
+    else
+      super method_name, *args, &block
+    end
+  end
 end
 
 # The proxy object should pass the following Koan:
